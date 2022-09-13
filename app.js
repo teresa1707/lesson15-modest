fdd06341de901439eaa67ceadc5da7cee30e7854
Teresa <teriv1707@gmail.com>
2022-09-13 19:10:18 +0200

    error searching

--- app.js
+++ app.js
@@ -3,16 +3,17 @@
 const modal = document.querySelector(".modal");
 const sendMessage = document.querySelector(".sendMessage");
 const closeBtn = document.querySelector(".btn-close");
-console.log(closeBtn, modal, sendMessage);
+const modalBtn = document.querySelector(".modal-btn");
+console.log(closeBtn, modal, sendMessage, modalBtn);
 
 function openModal() {
-  modal.classList.add("show");
-  modal.classList.remove("hide");
+    modal.classList.add("show");
+    modal.classList.remove("hide");
 }
 
 function closeModal() {
-  modal.classList.add("hide");
-  modal.classList.remove("show");
+    modal.classList.add("hide");
+    modal.classList.remove("show");
 }
 
 sendMessage.addEventListener("click", openModal);
@@ -20,20 +21,20 @@ sendMessage.addEventListener("click", openModal);
 closeBtn.addEventListener("click", closeModal);
 
 modal.addEventListener("click", function (e) {
-  if (e.target === modal) {
-    closeModal();
-  }
+    if (e.target === modal) {
+        closeModal();
+    }
 });
 
 function showModalByScroll() {
-  // console.log(window.pageYOffset);
-  // console.log(document.body.scrollHeight / 2);
+    // console.log(window.pageYOffset);
+    // console.log(document.body.scrollHeight / 2);
 
-  if (window.pageYOffset > document.body.scrollHeight / 2) {
-    openModal();
-    //zabraty scroll
-    window.removeEventListener("scroll", showModalByScroll);
-  }
+    if (window.pageYOffset > document.body.scrollHeight / 2) {
+        openModal();
+        //zabraty scroll
+        window.removeEventListener("scroll", showModalByScroll);
+    }
 }
 window.addEventListener("scroll", showModalByScroll);
 
@@ -43,12 +44,27 @@ const mobileMenu = document.querySelector(".nav-mobile-menu");
 const mainMenu = document.querySelector(".navigation");
 
 mobileMenu.addEventListener("click", function () {
-  mobileMenu.classList.toggle("active-menu");
-  if (mobileMenu.classList.contains("active-menu")) {
-    mainMenu.classList.add("active-menu");
-  } else {
-    mainMenu.classList.remove("active-menu");
-  }
+    mobileMenu.classList.toggle("active-menu");
+    if (mobileMenu.classList.contains("active-menu")) {
+        mainMenu.classList.add("active-menu");
+    } else {
+        mainMenu.classList.remove("active-menu");
+    }
 });
 
-//slider
+//social-media on photos
+
+const socials = document.querySelectorAll(".photo-hover-social-media");
+
+const figureImages = document.querySelectorAll(".face");
+console.log(socials, figureImages);
+
+for (let i = 0; i < figureImages.length; i++) {
+    figureImages[i].addEventListener("mouseover", function showSocial() {
+        socials[i].setAttribute("style", "display:block;");
+    });
+
+    figureImages[i].addEventListener("mouseout", function hideSocial() {
+        socials[i].setAttribute("style", "display:none;");
+    });
+}
